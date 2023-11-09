87794cefeb5bd0706960cc4ca11a596a8d5717a2
Argie <argieragasd8d7@gmail.com>
2023-11-09 17:47:25 +0800

    add marker on map

--- src/app/page/map/map.component.ts
+++ src/app/page/map/map.component.ts
@@ -6,22 +6,53 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit{
+  map: L.Map
+  markers: L.Layer[] = [];
+
+  popupText = "Some popup text";
+
+  markerIcon = {
+    icon: L.icon({
+      iconSize: [25, 41],
+      iconAnchor: [10, 41],
+      popupAnchor: [2, -40],
+      // specify the path here
+      iconUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png",
+      shadowUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png"
+    })
+  };
+
   options = {
     layers: [
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 57,
+        attribution: ""
       })
     ],
     zoom: 10,
     center: L.latLng(6.930830, 126.280320)
+  };
+
+  addMarker() {
+    const newMarker = L.marker([6.930830, 126.280320], this.markerIcon);
+
+    this.markers.push(newMarker);
+
+    newMarker.addTo(this.map);
   }
 
+  onMapReady(map: L.Map) {
+    this.map = map;
+    // Do stuff with map
+  }
   open = false
 
   ngOnInit(): void {
     setTimeout(() => {
       this.open = true
     }, 100);
+    setTimeout(()=>{
+      this.addMarker()
+    }, 500)
   }
 }
